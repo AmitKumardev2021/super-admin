abcc95d2d7f9e13b3d4ed1b37645e9a90775ebcb
devjs1000 <atim4077@gmail.com>
2022-05-10 14:02:52 +0530

    errors resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React, { lazy } from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import AccessibleNavigationAnnouncer from './components/AccessibleNavigationAnnouncer'
+import React, { lazy } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import AccessibleNavigationAnnouncer from "./components/AccessibleNavigationAnnouncer";
 
-const Layout = lazy(() => import('./containers/Layout'))
-const Login = lazy(() => import('./pages/Login'))
-const CreateAccount = lazy(() => import('./pages/CreateAccount'))
-const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
+const Layout = lazy(() => import("./containers/Layout"));
+const Login = lazy(() => import("./pages/Login"));
+const Heritage = lazy(() => import("./pages/Heritage"));
+const CreateAccount = lazy(() => import("./pages/CreateAccount"));
+const ForgotPassword = lazy(() => import("./pages/ForgotPassword"));
 
 function App() {
   return (
@@ -14,9 +20,9 @@ function App() {
         <AccessibleNavigationAnnouncer />
         <Switch>
           <Route path="/login" component={Login} />
-          <Route path="/create-account" component={CreateAccount} />
-          <Route path="/forgot-password" component={ForgotPassword} />
-
+          {/* <Route path="/create-account" component={CreateAccount} /> */}
+          {/* <Route path="/forgot-password" component={ForgotPassword} /> */}
+          <Route path="/heritage" component={Heritage} />
           {/* Place new routes over this */}
           <Route path="/app" component={Layout} />
           {/* If you have an index page, you can remothis Redirect */}
@@ -24,7 +30,7 @@ function App() {
         </Switch>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Heritage.jsx
+++ src/pages/Heritage.jsx
@@ -3,14 +3,13 @@ import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 //create an ckeditor instance
-export default Heritage = (props: any) => {
+export default (props) => {
   return (
     <div className="">
       <CKEditor
         editor={ClassicEditor}
-
         data="<p>Hello from CKEditor 5!</p>"
-        onChange={(event: any, editor: { getData: () => any }) => {
+        onChange={(event, editor) => {
           const data = editor.getData();
           props.setArticle(data);
         }}
